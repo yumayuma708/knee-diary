7d4a62ea827262b8d6c25454382944a27bcc7cb2
yumayuma708 <yumaskywalker708@gmail.com>
2024-02-15 23:37:25 +0900

    かわいいフォントに変更

--- app/src/main/java/com/example/kneediary/MainActivity.kt
+++ app/src/main/java/com/example/kneediary/MainActivity.kt
@@ -7,12 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.example.kneediary.ui.screens.start_screen.StartScreen
 import com.example.kneediary.ui.theme.KneeDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val provider = GoogleFont.Provider(
+            providerAuthority = "com.google.android.gms.fonts",
+            providerPackage = "com.google.android.gms",
+            certificates = R.array.com_google_android_gms_fonts_certs
+        )
         setContent {
             KneeDiaryTheme {
                 Surface(
