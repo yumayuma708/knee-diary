1c90562303ba645c5f440bdfe1d2c3554d3ca26c
yumayuma708 <yumaskywalker708@gmail.com>
2024-04-09 18:45:36 +0900

    ファイルを分ける

--- app/src/main/java/com/example/kneediary/MainActivity.kt
+++ app/src/main/java/com/example/kneediary/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.KneeDiaryTheme
-import com.example.kneediary.navigation.Nav
-import com.example.kneediary.ui.screens.navigated_screen.home_screen.HomeScreen
-import com.example.kneediary.ui.screens.start_screen.StartScreen
+import com.example.kneediary.navigation.KneeDiaryNavHost
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             KneeDiaryTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Nav.StartScreen.name) {
-                    composable(route = Nav.StartScreen.name) {
-                        StartScreen(
-                            onNavigateToHomeScreen = {
-                                navController.navigate(Nav.HomeScreen.name)
-                            },
-                        )
-                    }
-                    composable(route = Nav.HomeScreen.name) {
-                        HomeScreen(
-                        )
-                    }
-                }
+                KneeDiaryNavHost(navController = navController)
             }
         }
     }
--- app/src/main/java/com/example/kneediary/navigation/KneeDiaryNavHost.kt
+++ app/src/main/java/com/example/kneediary/navigation/KneeDiaryNavHost.kt
@@ -1,18 +1,28 @@
 package com.example.kneediary.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.kneediary.ui.screens.navigated_screen.home_screen.HomeScreen
+import com.example.kneediary.ui.screens.start_screen.StartScreen
 
-//@Composable
-//fun KneeDiaryNavHost(
-//    navController: NavHostController = rememberNavController(),
-//    startDestination: String = "home"
-//) {
-//    NavHost(navController = navController, startDestination = startDestination) {
-//        composable("home") {
-//            Greeting("Android")
-//        }
-//    }
-//}
+@Composable
+fun KneeDiaryNavHost(
+    navController: NavHostController = rememberNavController(),
+) {
+    NavHost(navController = navController, startDestination = Nav.StartScreen.name) {
+                    composable(route = Nav.StartScreen.name) {
+                        StartScreen(
+                            onNavigateToHomeScreen = {
+                                navController.navigate(Nav.HomeScreen.name)
+                            },
+                        )
+                    }
+                    composable(route = Nav.HomeScreen.name) {
+                        HomeScreen(
+                        )
+                    }
+                }
+}
