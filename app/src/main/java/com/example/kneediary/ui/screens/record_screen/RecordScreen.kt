fb7d31402df048a13dff0115f0befcff1d7c2c88
yumayuma708 <yumaskywalker708@gmail.com>
2024-04-11 19:31:30 +0900

    右足、左足を選択できるようにする

--- app/src/main/java/com/example/kneediary/ui/screens/record_screen/RecordScreen.kt
+++ app/src/main/java/com/example/kneediary/ui/screens/record_screen/RecordScreen.kt
@@ -1,20 +1,32 @@
 package com.example.kneediary.ui.screens.record_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.kneediary.navigation.Nav
@@ -24,44 +36,50 @@ import com.example.kneediary.navigation.Nav
 fun RecordScreen(
     navController: NavHostController,
 ) {
+    var state by remember { mutableStateOf(true) }
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Box {
-                            Text("痛みを記録する")
-                        }
-                        Box(
-                            modifier = Modifier
-                                .align(Alignment.TopEnd)
-                                .padding(end = 16.dp)
-                        ) {
-                            IconButton(
-                                onClick = {
-                                    navController.navigate(Nav.HomeScreen.name)
-                                }) {
-                                Icon(Icons.Rounded.Close, contentDescription = "閉じる")
-                            }
-                        }
-                        Box(
-                            modifier = Modifier
-                                .align(Alignment.BottomEnd)
-                        ) {
-                        Divider()
-                        }
-                    }
+                    Text(
+                        "痛みを記録する",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.CenterHorizontally),
+                        style = MaterialTheme.typography.titleLarge,
+                    )
                 },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            navController.navigate(Nav.HomeScreen.name)
+                        }) {
+                        Icon(Icons.Rounded.Close, contentDescription = "閉じる")
+                    }
+                }
             )
         },
         content = { paddingValues ->
             Box(
-                modifier = Modifier.padding(paddingValues)
+                contentAlignment = Alignment.Center
             ) {
-
+                Column(
+                    modifier = Modifier.padding(paddingValues),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Divider()
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        RadioButton(selected = state, onClick = { state = true })
+                        Text("左足")
+                        Box(modifier = Modifier.size(width = 20.dp, height = 20.dp))
+                        RadioButton(selected = !state, onClick = { state = false })
+                        Text("右足")
+                    }
+                }
             }
         }
     )
