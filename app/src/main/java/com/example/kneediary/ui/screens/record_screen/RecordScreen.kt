e649f99857cc4b65285cc68588a21097b322345d
yumayuma708 <yumaskywalker708@gmail.com>
2024-04-12 14:30:04 +0900

    日付を取得できるようにする。

--- app/src/main/java/com/example/kneediary/ui/screens/record_screen/RecordScreen.kt
+++ app/src/main/java/com/example/kneediary/ui/screens/record_screen/RecordScreen.kt
@@ -1,10 +1,12 @@
 package com.example.kneediary.ui.screens.record_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -13,8 +15,13 @@ import androidx.compose.material.icons.filled.AcUnit
 import androidx.compose.material.icons.filled.Cloud
 import androidx.compose.material.icons.filled.Umbrella
 import androidx.compose.material.icons.filled.WbSunny
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Event
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,6 +31,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,9 +40,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.kneediary.navigation.Nav
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -56,6 +72,18 @@ fun RecordScreen(
     val icons =
         listOf(Icons.Filled.WbSunny, Icons.Filled.Cloud, Icons.Filled.Umbrella, Icons.Filled.AcUnit)
     val iconIds = listOf(0, 1, 2, 3)
+    val datePickerState = rememberDatePickerState()
+    val now = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"))
+    val df = DateTimeFormatter.ofPattern("yyyy年MM月dd日")
+    val fdate = df.format(now)
+    val formattedDate: String = datePickerState.selectedDateMillis?.let {
+        val localDate = Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDate()
+        localDate.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日"))
+    } ?: fdate
+    var showDialog by remember { mutableStateOf(false) }
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -116,10 +144,10 @@ fun RecordScreen(
                     )
                     Text(text = ((pain * 4).roundToInt() + 1).toString())
                     Box(modifier = Modifier.size(width = 20.dp, height = 30.dp))
-                    Row (
+                    Row(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceEvenly,
-                    ){
+                    ) {
                         iconIds.forEach { id ->
                             IconButton(
                                 onClick = {
@@ -138,10 +166,80 @@ fun RecordScreen(
                             }
                         }
                     }
+                    Box(modifier = Modifier.size(width = 20.dp, height = 30.dp))
+                    Row(
+                        modifier = Modifier
+                            .clickable(
+                                onClick = { showDialog = true }
+                            )
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start,
+                    ) {
+                        val commonSize = Modifier.size(35.dp)
+                        Icon(
+                            imageVector = Icons.Rounded.Event,
+                            contentDescription = "カレンダー",
+                            modifier = commonSize
+                        )
+                        Box(modifier = Modifier.size(width = 20.dp, height = 30.dp))
+                        Text(
+                            formattedDate,
+                            style = MaterialTheme.typography.titleMedium,
+                            maxLines = 1
+                        )
+                        Icon(
+                            imageVector = Icons.Rounded.ArrowDropDown,
+                            contentDescription = "表示",
+                            modifier = commonSize
+                        )
+                        if (showDialog) {
+                            AlertDialog(
+                                onDismissRequest = {
+                                    showDialog = false
+                                },
+                                text = {
+                                    DatePicker(
+                                        datePickerState = datePickerState,
+                                        modifier = Modifier.fillMaxWidth()
+                                    )
+                                },
+                                confirmButton = {
+                                    Button(
+                                        onClick = {
+                                            showDialog = false
+                                        }
+                                    ) {
+                                        Text("OK")
+                                    }
+                                },
+                                dismissButton = {
+                                    Button(
+                                        onClick = {
+                                            showDialog = false
+                                        }
+                                    ) {
+                                        Text("キャンセル")
+                                    }
+                                },
+                                properties = DialogProperties(
+                                    usePlatformDefaultWidth = false
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth(0.99f)
+                                    .height(screenHeight * 0.75f)
+                            )
+                        }
+                    }
                 }
             }
         }
     )
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun PreviewRecordScreen() {
+    val navController = rememberNavController()
+    RecordScreen(navController = navController)
+}
