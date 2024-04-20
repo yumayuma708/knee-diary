9c8d9ab62867e3587eec8a6e7937552000e83c66
yumayuma708 <yumaskywalker708@gmail.com>
2024-04-20 13:44:42 +0900

    コードの説明のコメントアウトを追加(自分の勉強のため)

--- app/src/main/java/com/example/kneediary/ui/screens/record_screen/record_screen/RecordScreenViewModel.kt
+++ app/src/main/java/com/example/kneediary/ui/screens/record_screen/record_screen/RecordScreenViewModel.kt
@@ -15,6 +15,9 @@ import javax.inject.Inject
 class RecordScreenViewModel @Inject constructor(
     private val kneeRecordRepository: KneeRecordRepository
 ): ViewModel() {
+    //sealedがついているため、このインターフェイスの使用がこのモジュール内に制限される。
+    //また、このインターフェイスないの全ての可能性をコンパイラが考慮する。
+    //例. when式で全ての可能性を網羅していない場合、コンパイルエラーが発生する。
     sealed interface UiState {
         //Idleは入力を受け付けている状態
         data object Idle : UiState
@@ -24,10 +27,11 @@ class RecordScreenViewModel @Inject constructor(
         //CreateErrorは、例外の内容を持っておくと便利なので、data objectではなくdata classで作る
         data class CreateError(val e: Exception) : UiState
     }
-
+    //()の中が、初期化時点での値。つまり、UiStateの初期値は入力待ち状態
     private val _uiState = MutableStateFlow<UiState>(UiState.Idle)
 
     //Compose側から勝手に値の書き換えができないよう、uiStateをStateFlowとして作る
+    //.asStateFlowで、MutableStateFlowをStateFlowに変換している
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
     fun create(
         date: Long,
@@ -39,6 +43,7 @@ class RecordScreenViewModel @Inject constructor(
     ) {
         if (weather.isEmpty()) {
             _uiState.value = UiState.InputError
+            //returnは、create関数からの脱出を表す。この場合、InputErrorの時点でcreate関数を終了する。
             return
         }
         //KneeRecordRepositoryのcreate()を呼び出す
