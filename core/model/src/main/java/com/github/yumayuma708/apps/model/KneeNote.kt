b041a2612bc03f4201d6bc40e1225dc28398a306
yumayuma708 <yumaskywalker708@gmail.com>
2024-05-09 09:46:27 +0900

    data classを定義する

---
+++ core/model/src/main/java/com/github/yumayuma708/apps/model/KneeNote.kt
@@ -0,0 +1,9 @@
+package com.github.yumayuma708.apps.model
+
+data class KneeNote (
+    val id: Long,
+    val title: String,
+    val description: String,
+    val createdAt: Long,
+    val updatedAt: Long,
+)
