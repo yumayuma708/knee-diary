5c624ea6275dce7c89cb479d49dc3449f1342c73
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-28 17:26:45 +0900

    Compose Compiler を有効にする kotlinCompilerExtensionVersion を "1.5.14"に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     // 1.5.1かも?
     packagingOptions {
